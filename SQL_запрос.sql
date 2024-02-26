11b2907d942459d2af9f716456bec1a6077172d2
Ivan Obraztcov <120329237+ChillyWilly59@users.noreply.github.com>
2024-02-26 19:23:05 +0400

    Add SQL request

---
+++ SQL_запрос.sql
@@ -0,0 +1,7 @@
+﻿SELECT
+  p.Name AS 'Имя продукта',
+  c.Name AS 'Имя категории'
+FROM Products p
+LEFT JOIN ProductCategories pc ON p.Id = pc.ProductId
+LEFT JOIN Categories c ON pc.CategoryId = c.Id
+ORDER BY p.Name;
